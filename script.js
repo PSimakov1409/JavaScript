8fe699c75be5bfd80005a96ae63c1eecf2a98d5d
PSimakov1409 <PSimakov1409@gmail.com>
2015-01-21 16:46:07 +0300

    Добавлен рандом и очистка формы

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var m = new Array()
-m.test = 6
-m[1] = "Виктор Крылов"
-m[2] = "Евгений Хашин"
-m[3] = "Лидия Хашина"
-m[4] = "Сергей Авдеев"
-m[5] = "Антон Сысоев"
-m[6] = "Павел Симаков"
+var m = new Array( "Виктор Крылов", "Евгений Хашин", "Лидия Хашина","Сергей Авдеев","Антон Сысоев","Павел Симаков")
 $(document).ready(function(){
-	var mylist = $("#list");
-	for (mylist = 0; mylist < 6; mylist++){
-		mylist.append("<li>" + m[mylist] + "</li>")
+	$('button').each(function(){
+		$(this).click(function(){
+			var n = 5;
+			var i = $("#list");
+			$("#list").html("");
+    		for (j = 0; j < 6; j++){
+				var x = Math.floor(Math.random()*(n+1));
+				i.append("<li>" + m[x] + "</li>");
+			}
+		})
+	})
+	var i = $("#list");
+	for (j = 0; j < 6; j++){
+		i.append("<li>" + m[j] + "</li>");
 	}
-})
+	
+});
