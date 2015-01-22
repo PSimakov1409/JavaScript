894d54ad2315df8c2fdda17f7144cbf17ee148f9
PSimakov1409 <PSimakov1409@gmail.com>
2015-01-22 12:22:37 +0300

    Sort

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
+function ASort (a,b) {return a-b;}
 var m = new Array( "Виктор Крылов", "Евгений Хашин", "Лидия Хашина","Сергей Авдеев","Антон Сысоев","Павел Симаков")
 $(document).ready(function(){
-	$('button').each(function(){
-		$(this).click(function(){
-			var n = 5;
-			var i = $("#list");
-			$("#list").html("");
-    		for (j = 0; j < 6; j++){
-				var x = Math.floor(Math.random()*(n+1));
-				i.append("<li>" + m[x] + "</li>");
-			}
-		})
-	})
-	var i = $("#list");
-	for (j = 0; j < 6; j++){
-		i.append("<li>" + m[j] + "</li>");
-	}
-	
+  $("#btn1").each(function(){
+    $(this).click(function(){
+      var n = 5;
+      var i = $("#list");
+      $("#list").html("");
+        for (j = 0; j < 6; j++){
+        var x = Math.floor(Math.random()*(n+1));
+        i.append("<li>" + m[x] + "</li>");
+      }
+    })
+  })
+  $("#btn2").each(function(){
+    $(this).click(function(){
+      var mylist = $('#list');
+      var listitems = mylist.children('li').get();
+      listitems.sort(function(a, b) {
+         var compA = $(a).text();
+         var compB = $(b).text();
+         return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+      })
+      $.each(listitems, function(idx, itm){
+        mylist.append(itm); 
+      });
+    })
+  })
+  $("#btn3").each(function(){
+    $(this).click(function(){
+      var mylist = $('#list');
+      var listitems = mylist.children('li').get();
+      listitems.sort(function(a, b) {
+         var compA = $(a).text();
+         var compB = $(b).text();
+         return (compA > compB) ? -1 : (compA < compB) ? 1 : 0;
+      })
+      $.each(listitems, function(idx, itm){
+        mylist.append(itm); 
+      });
+    })
+  })
+  var i = $("#list");
+  for (j = 0; j < 6; j++){
+    i.append("<li>" + m[j] + "</li>");
+  }
 });
